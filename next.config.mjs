fec69c3c8bcd9707210875df948e388948b85025
nacil420 <ramdhanihw420@gmail.com>
2024-09-10 20:41:42 +0700

    fix error deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-// const nextTranslate = require("next-translate");
-// const nextConfig = {
-//   i18n: {
-//     locales: ["en", "id"],
-//     defaultLocale: "id",
-//   },
-// };
 
 export default nextConfig;
